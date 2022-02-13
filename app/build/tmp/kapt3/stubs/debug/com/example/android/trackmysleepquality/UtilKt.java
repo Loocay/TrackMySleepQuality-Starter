01bcb39f4961bc6628f31909813829b7c4cf1c5b
Kaskarauov Adilet <kotik200w@mail.ru>
2022-02-13 14:38:32 +0600

    Initial commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/trackmysleepquality/UtilKt.java
@@ -0,0 +1,26 @@
+package com.example.android.trackmysleepquality;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000,\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\t\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\u001a\u0010\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007\u001a\u0016\u0010\u0004\u001a\u00020\u00012\u0006\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\b\u001a\u001c\u0010\t\u001a\u00020\n2\f\u0010\u000b\u001a\b\u0012\u0004\u0012\u00020\r0\f2\u0006\u0010\u0007\u001a\u00020\b\u00a8\u0006\u000e"}, d2 = {"convertLongToDateString", "", "systemTime", "", "convertNumericQualityToString", "quality", "", "resources", "Landroid/content/res/Resources;", "formatNights", "Landroid/text/Spanned;", "nights", "", "Lcom/example/android/trackmysleepquality/database/SleepNight;", "app_debug"})
+public final class UtilKt {
+    
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String convertNumericQualityToString(int quality, @org.jetbrains.annotations.NotNull()
+    android.content.res.Resources resources) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @android.annotation.SuppressLint(value = {"SimpleDateFormat"})
+    public static final java.lang.String convertLongToDateString(long systemTime) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public static final android.text.Spanned formatNights(@org.jetbrains.annotations.NotNull()
+    java.util.List<com.example.android.trackmysleepquality.database.SleepNight> nights, @org.jetbrains.annotations.NotNull()
+    android.content.res.Resources resources) {
+        return null;
+    }
+}
